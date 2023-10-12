b0a77208c18e3a3b7bfd7808acb896457190150a
CodingForEntrepreneurs <hello@teamcfe.com>
2023-10-12 15:32:26 -0600

    14 - API Route for Using SST Secrets

---
+++ src/app/api/route.js
@@ -0,0 +1,15 @@
+import {NextResponse} from 'next/server'
+import {Config} from 'sst/node/config'
+
+
+export const dynamic = 'force-dynamic'
+export const runtime = 'nodejs' 
+export const fetchCache = 'force-no-store'
+export const revalidate = 0
+
+export async function GET(request){
+    const secretVal = Config.SECRET_VAL
+    const stage = Config.STAGE
+    return NextResponse.json({hello: "World", stage: stage, secretVal: secretVal}, {status: 200})
+
+}
