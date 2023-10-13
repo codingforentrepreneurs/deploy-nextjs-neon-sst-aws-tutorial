50b4f997cdaea66a6924717424c7eefe5a061731
CodingForEntrepreneurs <hello@teamcfe.com>
2023-10-13 11:03:45 -0600

    22 - Create Function to Insert Database Data

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,7 +1,7 @@
 import {NextResponse} from 'next/server'
 import {Config} from 'sst/node/config'
 
-import {dbNow} from '@/app/lib/db'
+import {dbNow, addLead} from '@/app/lib/db'
 
 export const dynamic = 'force-dynamic'
 export const runtime = 'nodejs' 
@@ -13,11 +13,13 @@ export async function GET(request){
     const dbString = Config.DATABASE_URL
     const stage = Config.STAGE
     const dbResult = await dbNow()
+    const leadResult = await addLead({email: "abc123@abc123.com"})
     const now = dbResult ? dbResult[0].now : null
     return NextResponse.json({
         hello: "World", 
         stage: stage, 
         secretVal: secretVal, 
+        leadResult: leadResult,
         dbString: `${dbString}`.slice(0, 25),
         now: now
     }, {status: 200})
--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,12 +1,28 @@
-import { neon } from '@neondatabase/serverless'
-
+import { neon, neonConfig } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import { Config } from 'sst/node/config'
 
-export async function dbClient () {
-    return neon(Config.DATABASE_URL)
+import * as schema from '@/app/lib/schema'
+
+export async function dbClient (useSQLOnly) {
+    neonConfig.fetchConnectionCache = true;
+    const sql = neon(Config.DATABASE_URL)
+    if (useSQLOnly) {
+        return sql
+    }
+    return drizzle(sql);
 }
 
 export async function dbNow () {
-    const sql = await dbClient()
+    const sql = await dbClient(true)
     return sql`SELECT NOW()`
+}
+
+export async function addLead({email}){
+    const db = await dbClient(false)
+    const dbResult =  await db.insert(schema.LeadTable).values({email: email}).returning({timestamp: schema.LeadTable.createdAt})
+    if (dbResult.length === 1) {
+        return dbResult[0].timestamp
+    }
+    return 
 }
