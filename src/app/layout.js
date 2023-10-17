49d5155ffa461db989ae729487fc533a6b29c7f5
CodingForEntrepreneurs <hello@teamcfe.com>
2023-10-17 12:03:24 -0600

    33 - Improved Visuals with Tailwind and Flowbite

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} px-4`}>{children}</body>
     </html>
   )
 }
--- src/app/leadCapture/forms.js
+++ src/app/leadCapture/forms.js
@@ -29,8 +29,8 @@ export default function LeadCaptureForm () {
 
     }
     const btnLabel = loading ? "Loading" : "Join List"
-    return <form className='space-x-3' onSubmit={handleForm}>
-        <input type='text' required name='email' placeholder="Your Email" />
-        <button disabled={loading} className='bg-green-500 hover:bg-green-700 text-white px-3 rounded' type='submit'>{btnLabel}</button>
+    return <form className='space-y-3' onSubmit={handleForm}>
+        <input type='email' required name='email' placeholder="Your Email" />
+        <button disabled={loading} className='btn-join' type='submit'>{btnLabel}</button>
     </form>
 }
